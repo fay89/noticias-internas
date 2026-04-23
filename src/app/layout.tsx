f8f0d9c192220d09091170afa594e8080f8a1823
Fay89 <abrunhuberbrunhuber@gmail.com>
2026-04-23 20:45:16 +0100

    Añadir botón de publicar en App y hacer el editor responsivo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -90,6 +90,13 @@ export default function RootLayout({
                     <span className={layoutStyles.mobileText}>Juegos</span>
                   </a>
                 </li>
+                <li>
+                  <a href="/admin/editor" className={layoutStyles.publishButton}>
+                    <svg className={layoutStyles.navIcon} viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M11 4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7"></path><path d="M18.5 2.5a2.121 2.121 0 0 1 3 3L12 15l-4 1 1-4 9.5-9.5z"></path></svg>
+                    <span className={layoutStyles.desktopText}>Redactar</span>
+                    <span className={layoutStyles.mobileText}>Publicar</span>
+                  </a>
+                </li>
               </ul>
             </nav>
           </div>
